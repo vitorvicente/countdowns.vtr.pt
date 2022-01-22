ec749172d5d5feabe3a46782c6b9eae51169a067
Victor Vicente <vitor@bu.edu>
2022-01-21 19:59:19 -0500

    Reducing Files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 import { withFirebase } from "vtr-react-components/dist/Firebase";
 import { VtrHeader } from "vtr-react-components/dist/VtrHeader";
@@ -9,7 +12,73 @@ import { VtrFooter } from "vtr-react-components/dist/VtrFooter";
 
 import { getDoc } from "firebase/firestore";
 
-import Card from "./components/Card";
+const StyledCard = styled.div`
+	text-align: center;
+	position: relative;
+	overflow: hidden;
+	border-radius: 5px;
+	margin: 0 10px 40px 10px;
+	box-shadow: 0 10px 29px 0 rgba(68, 88, 144, 0.1);
+	transition: all 0.3s ease-in-out;
+	&:hover {
+	    -webkit-transform: translateY(-5px);
+		transform: translateY(-5px);
+
+		
+		& h3 {
+			color: #007bff;
+			padding-bottom: 20px;
+			padding-top: 20px;
+		}
+	}
+
+	
+	& h3 {
+		padding-bottom: 20px;
+		padding-top: 20px;
+	}
+	
+	& h4 {
+		padding-bottom: 20px;
+		padding-top: 20px;
+		color: #007bff;
+	}
+	
+`;
+
+const Card = ({title, description, timeEnds}) => {
+	const [ days, setDays ] = useState("");
+	const [ hours, setHours ] = useState("");
+	const [ minutes, setMinutes ] = useState("");
+	const [ seconds, setSeconds ] = useState("");
+	const [ isDone, setIsDone ] = useState(true);
+
+	useEffect(() => {
+		setInterval(countDown, 1000);
+	});
+
+	const countDown = () => {
+		const difference = timeEnds.toDate().getTime() - (new Date()).getTime();
+
+		if (difference > 0)
+			setIsDone(false);
+
+		setDays(Math.floor(difference / (1000 * 60 * 60 * 24)).toString());
+		setHours(Math.floor((difference%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)).toString().padStart(2, '0'));
+		setMinutes(Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0'));
+		setSeconds(Math.floor((difference % (1000 * 60)) / 1000).toString().padStart(2, '0'));
+	};
+
+	return (
+		<Col>
+			<StyledCard>
+				<h3>{title}</h3>
+				<p> {description} </p>
+				{ !isDone ? <h4> {days} Days {hours}:{minutes}:{seconds} </h4> : <h4> Countdown Complete! </h4>}
+			</StyledCard>
+		</Col>
+	);
+}
 
 
 const App = ({ firebase }) => {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { FirebaseContext } from 'vtr-react-components/dist/Firebase';
+import { default as FirebaseSuper } from 'vtr-react-components/dist/Firebase';
+
 import App from './App';
 
-import Firebase from "./components/Firebase";
+const firebaseConfig = {
+    apiKey: "AIzaSyDWiUHWHAPft5TJZ7RbteUSr5pdvMIqs_c",
+    authDomain: "vtr-pt.firebaseapp.com",
+    databaseURL: "https://vtr-pt.firebaseio.com",
+    projectId: "vtr-pt",
+    storageBucket: "vtr-pt.appspot.com",
+    messagingSenderId: "363782155278",
+    appId: "1:363782155278:web:df68e732efd529d346205f",
+    measurementId: "G-L1ZGQDL204"
+};
+
+class Firebase extends FirebaseSuper {
+    constructor(app) {
+        super(app, firebaseConfig);
+    }
+}
 
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
